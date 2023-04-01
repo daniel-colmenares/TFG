45981402b90a53f9e19e109c04f7657d3d5ac492
daniel-colmenares <daniel.colmenares@udc.es>
2023-04-01 18:48:52 +0200

    calendario tabla

--- app/src/main/java/com/example/tfg/DBOpenHelper.java
+++ app/src/main/java/com/example/tfg/DBOpenHelper.java
@@ -14,22 +14,38 @@ public class DBOpenHelper extends SQLiteOpenHelper {
     private final static String CREATE_EVENTS_TABLE= "create table " +DBStructure.EVENT_TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, "
             +DBStructure.EVENT+ " TEXT, " + DBStructure.DATE+" TEXT, "+DBStructure.MONTH+" TEXT, "+DBStructure.YEAR+" TEXT, "+DBStructure.IMAGEN+" TEXT)";
 
+    private final static String CREATE_CALENDAR_TABLE= "create table " +DBStructure.CALENDAR_TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+            +DBStructure.NAME+ " TEXT, " + DBStructure.EMAIL+" TEXT)";
+
     private static final String DROP_EVENTS_TABLE="DROP TABLE IF EXISTS "+DBStructure.EVENT_TABLE_NAME;
+
+    private static final String DROP_CALENDAR_TABLE="DROP TABLE IF EXISTS "+DBStructure.CALENDAR_TABLE_NAME;
     public DBOpenHelper(@Nullable Context context) {
         super(context, DBStructure.DB_NAME,null, DBStructure.DB_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
         db.execSQL(CREATE_EVENTS_TABLE);
+        db.execSQL(CREATE_CALENDAR_TABLE);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL(DROP_EVENTS_TABLE);
+        db.execSQL(DROP_CALENDAR_TABLE);
         onCreate(db);
     }
 
+    public void SaveCalendar(String name, String email, SQLiteDatabase database){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DBStructure.NAME,name);
+        contentValues.put(DBStructure.EMAIL,email);
+        database.insert(DBStructure.CALENDAR_TABLE_NAME,null,contentValues);
+
+    }
     public void SaveEvent(String event, Uri uri, String date, String month, String year, SQLiteDatabase database){
         ContentValues contentValues = new ContentValues();
         contentValues.put(DBStructure.EVENT,event);
--- app/src/main/java/com/example/tfg/DBStructure.java
+++ app/src/main/java/com/example/tfg/DBStructure.java
@@ -4,6 +4,7 @@ import android.media.Image;
 import android.net.Uri;
 
 public class DBStructure {
+    //EVENTOS
     public static final String DB_NAME="EVENTS_DB";
     public static final int DB_VERSION=3;
     public static final String EVENT_TABLE_NAME="eventstable";
@@ -11,6 +12,12 @@ public class DBStructure {
     public static final String DATE="date";
     public static final String MONTH="month";
     public static final String YEAR="year";
-
     public static final String IMAGEN = "imagen";
+    //CALENDARIOS
+    public static final String CALENDAR_TABLE_NAME="calendartable";
+
+    public static final String EMAIL="email";
+
+    public static final String NAME="name";
+
 }
